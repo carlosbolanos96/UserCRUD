c812265228a0e07026b391d5ccb3f0401df6cc0b
carlosbolanos96 <carlosbolanos309@gmail.com>
2024-04-25 17:15:12 -0500

    crud completo

--- src/main/java/com/codeinnova/UserCRUD/controller/UsersControllers.java
+++ src/main/java/com/codeinnova/UserCRUD/controller/UsersControllers.java
@@ -3,11 +3,10 @@ package com.codeinnova.UserCRUD.controller;
 import com.codeinnova.UserCRUD.entities.Users;
 import com.codeinnova.UserCRUD.repository.UsersRepository;
 import org.apache.catalina.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,40 +14,34 @@ import java.util.Optional;
 @RestController
 public class UsersControllers {
 
-    private UsersRepository usersRepository;
+    private final Logger log = LoggerFactory.getLogger(UsersControllers.class);
 
+    private UsersRepository usersRepository;
     public UsersControllers(UsersRepository usersRepository) {
         this.usersRepository = usersRepository;
     }
 
     //CREATE
     @PostMapping ("/api/User")
-    public Users CreateUser (Users user){
-        usersRepository.save(user);
-        return user;
+    public ResponseEntity<Users> CreateUser (@RequestBody Users user){
+        if (user.getId()!=null){
+            log.warn("Unexpected id");
+            return ResponseEntity.badRequest().build();
+        }
+
+        Users response = usersRepository.save(user);
+        return ResponseEntity.ok(response);
     };
 
 
     //READ
+    //all
     @GetMapping("/api/Users")
     public List<Users> findAllUsers(){
         return usersRepository.findAll();
     };
 
-    //opc1
-    /*
-    @GetMapping("/api/User/{id}")
-    public Users findUser(@PathVariable Integer id){
-        Optional<Users> UserOpt = usersRepository.findById(id);
-
-        if (UserOpt.isPresent())
-            return UserOpt.get();
-        else
-            return null;
-        // o lo que seria lo mismo
-        // return UserOpt.orElse(null);
-    };*/
-
+    //read by id
     @GetMapping("/api/User/{id}")
     public ResponseEntity<Users> findUser(@PathVariable Integer id){
         Optional<Users> UserOpt = usersRepository.findById(id);
@@ -56,9 +49,37 @@ public class UsersControllers {
         if (UserOpt.isPresent())
             return ResponseEntity.ok(UserOpt.get());
         else
+            log.warn ("buscando un usuario inexistente");
             return ResponseEntity.notFound().build();
 
         // o lo que seria lo mismo
         //return UserOpt.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    };
+
+    //Update
+    @PutMapping (("/api/User"))
+    public ResponseEntity<Users> UpdateUser(@RequestBody Users user) {
+        if (user.getId() == null) {
+            log.warn("Expected Id");
+            return ResponseEntity.badRequest().build();
+        };
+
+        if (!usersRepository.existsById(user.getId())) {
+            log.warn("Trying to update a non exiist user");
+            return ResponseEntity.notFound().build();
+        };   ;
+
+        Users result = usersRepository.save(user);
+        return ResponseEntity.ok(result);
+    };
+
+    @DeleteMapping("/api/User/{id}")
+    public ResponseEntity<Users> deleteUser (@PathVariable Integer id){
+        if (!usersRepository.existsById(id)){
+            log.warn(("Not found user to delete"));
+            return ResponseEntity.notFound().build();
+        };
+        usersRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     };
 }
